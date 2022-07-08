ef3c4b8947ded09bacbd01c83cb98f2cc271ef7e
RejoiceRejoice <105215752+RejoiceRejoice@users.noreply.github.com>
2022-07-08 20:31:47 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+alert('My name is Rejoice Ekong Udoh');
+
+alert('5.8ft');
+
+alert('Nigeria');
+
+
+
+var name = ('My name is Rejoice Ekong Udoh');
+
+var height = ('5.8ft');
+
+var country = ('Nigeria');
+
+
+
+console.log(name);
+
+console.log(height);
+
+console.log(country);
